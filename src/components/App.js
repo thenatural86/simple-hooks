e9a1cd758d4c73f88ad3b6dd326783df648b2520
thenatural86 <philip.smalls86@gmail.com>
2020-04-03 11:50:45 -0400

    ResourceList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react"
+import ResourceList from "./ResourceList"
 
 const App = () => {
+  // array destructuring
+  // useState will return an array with two elements inside of it. Take the first element out of array and assign it to resource and second element inside of array is assigned to setResource
+
+  // [resource] = contains the present value of this piece of state - this.state.resource
+  // [setResource] = setter function to  call when we update our state and re-render - this.setState({resources: 'posts'})
+  // useState = function from react
+  // ('posts') === (initial value) = starting value for this piece of state, similar to when we initialize    out state object - state = {resources: 'posts'}
   const [resource, setResource] = useState("posts")
 
+  console.log(useState)
   return (
     <div>
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
-        {resource}
+        <ResourceList resource={resource} />
       </div>
     </div>
   )
