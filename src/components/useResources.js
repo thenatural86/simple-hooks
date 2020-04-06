d218a0f5c176a67040dc60e8076e43183e490abf
thenatural86 <philip.smalls86@gmail.com>
2020-04-06 13:07:54 -0400

    useEffect hook update

--- src/components/useResources.js
+++ src/components/useResources.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 const useResources = (resource) => {
-  // initialization of piece of state (resource) and function that to setState (setResources)
+  // initialization of piece of state (resource) and function that to setState (setResources).
+  // initialize value state to an empty array.
+  // have access to that state on the variable resources.
+  // have ability to change that piece of state using the setResources function.
   const [resources, setResources] = useState([])
 
-  // anytime component gets rendered/re-rendered to the screen we run inner function
+  // make a fetch request inside functional component to get resources
+  // also used to update resources
   useEffect(() => {
     ;(async (resource) => {
       const response = await axios.get(
